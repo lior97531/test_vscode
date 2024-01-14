ca13d5ef7368e519b9d9083cccf014740b9178a0
lior97531 <145993759+lior97531@users.noreply.github.com>
2024-01-14 10:53:52 +0200

    j

---
+++ try.java
@@ -0,0 +1,12 @@
+ class Class{
+     public static void main (String[] args){
+        
+        String [] cars = {"vooooooooolvo","d"};
+
+        System.out.println (cars[0]);
+
+        System.out.println (cars[1]);
+
+    }
+
+}    

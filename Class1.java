c85cc79bae240a44b281eebc10362fcd4fd2b41a
lior97531 <145993759+lior97531@users.noreply.github.com>
2024-01-28 08:30:45 +0000

    hi

---
+++ Class1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Class1 {
+Scanner scan = new Scanner(System.in);
+    int a;
+
+    public Class1(int b){
+
+        a= b;
+    }
+
+    public String toString(){
+return (a+"is inside");
+
+    }
+
+}
--- try.java
+++ try.java
@@ -1,11 +1,13 @@
+import java.util.Scanner;
  class Class{
      public static void main (String[] args){
+        Scanner scan = new Scanner(System.in);
+int a =scan.nextInt();
+
+        Class1 T = new Class1(a);
+
+        System.out.println(T);
         
-        String [] cars = {"vooooooooolvo","d"};
-
-        System.out.println (cars[0]);
-
-        System.out.println (cars[1]);
 
     }
 

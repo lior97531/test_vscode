bef4c23617e41535f923d1f07baf6af3d973bca5
lior97531 <145993759+lior97531@users.noreply.github.com>
2024-05-21 11:20:19 +0000

    leason 2 override and abstract and polimorfizem

---
+++ academic.java
@@ -0,0 +1,7 @@
+public class academic extends person{
+
+    public void work(){
+        super.work();  //to use the work method of the parent
+        System.out.println("smart");
+    }
+}
--- try.java
+++ try.java
@@ -2,11 +2,8 @@ import java.util.Scanner;
  class Class{
      public static void main (String[] args){
         Scanner scan = new Scanner(System.in);
-int a =scan.nextInt();
-
-        Class1 T = new Class1(a);
-
-        System.out.println(T);
+   academic a = new academic();
+   a.work();
         
 
     }

5578e514698fcd6e6c1a0fc569465dc11c6c7792
lior97531 <145993759+lior97531@users.noreply.github.com>
2025-04-15 12:55:46 +0000

    1

--- hello.py
+++ hello.py
@@ -1,5 +1,23 @@
 print("Hello World")
 
 arr=[[0.1,0,1],[-1,0.1,0],[0,-1,1]]
-for i in range(0,2)
-print(arr[i])
+
+
+new_col = [1, 2, 3]
+
+# Add the new column
+for i in range(len(arr)):
+    arr[i].append(new_col[i])
+
+print(arr)
+
+for i in range(1,3):
+    for j in range(0,4):
+       if(arr[i-1][j]!=0): 
+        arr[i][j]=abs(arr[i][j])-(abs(arr[i][j])/abs(arr[i-1][j]))*abs(arr[i-1][j])
+
+for row in arr:
+    print(row)
+
+    x1 = arr[3][4]/arr[3][0]
+    
